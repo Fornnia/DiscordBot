4c9b5a234532f966cedba65c91a8388d0cd6a90f
Fornnia <157953789+Fornnia@users.noreply.github.com>
2024-03-03 14:57:01 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord, os, requests
+from discord.ext import commands
+from discord.ext.commands import bot
+from config import TOKEN
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    
+@bot.command()
+async def ping(ctx: commands.Context):
+    await ctx.send('Pong!')
+    
+@bot.command() 
+async def hello(ctx): 
+    author = ctx.message.author 
+    await ctx.send(f'Привет, {author.mention}!  ⸜(｡˃ ᵕ ˂ )⸝♡') 
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command()
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+    
+bot.run(TOKEN)
